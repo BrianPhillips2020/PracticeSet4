de32575b2e7e3a488d5ee24ebb2e52bcefda60d0
brian <brian@128.4.106.70>
2017-04-04 16:00:08 -0400

    Final Commit

--- PracticeSet4/src/ch/makery/address/MainApp.java
+++ PracticeSet4/src/ch/makery/address/MainApp.java
@@ -114,7 +114,7 @@ public class MainApp extends Application {
         try {
             // Load person overview.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+            loader.setLocation(MainApp.class.getResource("view//PersonOverview.fxml"));
             AnchorPane personOverview = (AnchorPane) loader.load();
 
             // Set person overview into the center of root layout.
